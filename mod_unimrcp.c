789a6f6c8be44c851e1bc1329b9cb42e0707199c
Daniel Slavetskiy <daniel.slavetskiy@lekta.ai>
2025-02-21 16:07:35 +0100

    improve

--- mod_unimrcp.c
+++ mod_unimrcp.c
@@ -2498,7 +2498,6 @@ static switch_status_t recog_channel_enable_grammar(speech_channel_t *schannel, 
 			switch_log_printf(SWITCH_CHANNEL_UUID_LOG(schannel->session_uuid), SWITCH_LOG_DEBUG, "(%s) Enabling grammar %s\n", schannel->name, grammar_name);
 			switch_core_hash_insert(r->enabled_grammars, grammar_name, grammar);
 		}
-
 		switch_mutex_unlock(schannel->mutex);
 	}
 
